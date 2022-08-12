6ff1b49a4d8418789852fd0a8b2262fd4d1d90ba
heyuantao <he_yuan_tao@163.com>
2022-08-12 20:43:33 +0800

    'deploy'

--- media/guest/src/pages/DisplayPage/TodayCourseDisplay.js
+++ media/guest/src/pages/DisplayPage/TodayCourseDisplay.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
-import {Row, Col, Form, Input, Button, Alert, Icon, Layout, Menu, Select, DatePicker, Table} from "antd";
+import {Row, Col, Form, Input, Button, Alert, Icon, Layout, Menu, Select, DatePicker, Table, Progress} from "antd";
 import { Link,hashHistory } from "react-router";
 import { fromJS } from "immutable";
 import Settings from "../../Settings";
@@ -18,15 +18,18 @@ class TodayCourseDisplay extends React.Component {
             fetching: false,
             tableData: fromJS([]),
             displayTableData: fromJS([]),
+            displayRatio: 0,
         }
     }
     componentDidMount() {
         const centerUrl = Settings.centerAPIURL;
-        const page_size = 10;
+        const page_size = 16;
         let current_page = 1;
         let total_item;
         let total_page;
-        this.interval = setInterval(() => {
+        req.get(centerUrl, {}).then((response) => { this.setState({ centers: fromJS(response.data) }) });
+
+        this.displayTimer = setInterval(() => {
             total_item = this.state.tableData.size;
             total_page = Math.floor(total_item/page_size);
             if(current_page>total_page){
@@ -35,24 +38,24 @@ class TodayCourseDisplay extends React.Component {
             this.repeatDisplayContent(current_page,page_size,total_item);
             current_page=current_page+1;
         }, 3000);
-        req.get(centerUrl, {}).then((response) => { this.setState({ centers: fromJS(response.data) }) });
+        this.loadDataTimer = setInterval(()=>{this.fetchTableData();},10000);
     }
     componentWillUnmount() {
-        clearInterval(this.interval);
+        clearInterval(this.displayTimer);
+        clearInterval(this.loadDataTimer);
     }
     repeatDisplayContent(current_page,page_size,total_item){
         let display_content = fromJS([]);
         let begin = (current_page-1)*page_size+1
         let end = (current_page)*page_size;
+        const ratio = Math.floor(current_page/(total_item/page_size)*100)
         if(end>total_item){
             end=total_item;
         }
         display_content= this.state.tableData.slice(begin,end);
         this.setState({"displayTableData":display_content});
+        this.setState({"displayRatio":ratio});
 
-        console.log("Display!");
-        console.log(begin);
-        console.log(end);
     }
     handleFieldChange(value, field) {
         let dict = {}; dict[field] = value;
@@ -93,24 +96,30 @@ class TodayCourseDisplay extends React.Component {
             <Content style={{background: '#fff',minHeight: "850px", padding: "10px"}}>
                 <Row type="flex" justify="space-around" align="middle">
                     <Col span={22}>
-                        <Form  layout="inline">
-                            <Form.Item label={"实验中心"}>
-                                <Select value={formData.get("center_id")}  onChange={(v) => { this.handleFieldChange(v, "center_id") }}  style={{ width: 200 }}>
-                                    <Select.Option key={0} value={0}>未选择</Select.Option>
-                                    {
-                                       this.state.centers.map(function (obj) {
-                                          return <Select.Option key={obj.get("id")} value={obj.get("id")}>{obj.get("name")}</Select.Option>
-                                       })
-                                    }
-                                </Select>
-                            </Form.Item>
-                        </Form>
+                        <Row type="flex" justify="space-between" align="middle">
+                            <Col>
+                                <Form  layout="inline">
+                                    <Form.Item label={"实验中心"}>
+                                        <Select value={formData.get("center_id")}  onChange={(v) => { this.handleFieldChange(v, "center_id") }}  style={{ width: 200 }}>
+                                            <Select.Option key={0} value={0}>未选择</Select.Option>
+                                            {
+                                               this.state.centers.map(function (obj) {
+                                                  return <Select.Option key={obj.get("id")} value={obj.get("id")}>{obj.get("name")}</Select.Option>
+                                               })
+                                            }
+                                        </Select>
+                                    </Form.Item>
+                                </Form>
+                            </Col>
+                            <Col span={5}><Progress percent={this.state.displayRatio} size="small" /></Col>
+                        </Row>
                     </Col>
                 </Row>
                 <Row type="flex" justify="space-around" align="middle">
                     <Col span={22}>
                         <div style={{ marginBottom: "15px" }}></div>
-                        <Table dataSource={this.state.displayTableData.toJS()} rowKey="id"  columns={this.tableColumnFormat()}>
+                        <Table dataSource={this.state.displayTableData.toJS()} rowKey="id"  columns={this.tableColumnFormat()}
+                            pagination={false}>
                         </Table>
                     </Col>
                 </Row>
